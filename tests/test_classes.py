5f02cc8aacfcb3c4bde0ac4539626ce1df037541
Stepan Baidakov <stepanbaidakov@icloud.com>
2025-08-05 21:00:02 +0300

    homework 14.2 done

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -22,8 +22,7 @@ def category_devices(product_iphone):
 def test_category_init(category_devices):
     assert category_devices.name == "devices"
     assert category_devices.description == "мобильные устройства"
-    for product in category_devices.products:
-        assert isinstance(product, Product)
+    assert isinstance(category_devices.products, list)
 
 
 def test_product_count(product_iphone):
@@ -36,3 +35,47 @@ def test_category_count(product_iphone):
     Category.category_count = 0
     Category("devices", "мобильные устройства", [product_iphone])
     assert Category.category_count == 1
+
+
+@pytest.fixture
+def product_samsung():
+    return Product(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+    )
+
+
+def test_add_product(product_iphone, product_samsung,):
+    category_devices = Category("devices", "мобильные устройства", [product_iphone])
+    category_devices.add_product(product_samsung)
+    assert len(category_devices.products) == 2
+
+
+def test_products(category_devices):
+    assert category_devices.products == ["iPhone, 90000 руб. Остаток: 100 шт."]
+
+
+def test_new_product():
+    new_product = Product.new_product(
+        {
+            "name": "Samsung Galaxy S23 Ultra",
+            "description": "256GB, Серый цвет, 200MP камера",
+            "price": 180000.0,
+            "quantity": 5,
+        }
+    )
+    assert isinstance(new_product, Product)
+
+
+def test_price(product_samsung):
+    assert product_samsung.price == 180000.0
+
+
+def test_new_price_increases(product_samsung):
+    product_samsung.price = 280000
+    assert product_samsung.price == 280000
+
+
+def test_new_price_negative(product_samsung):
+    product_samsung.price = -100
+    assert "Цена не должна быть нулевая или отрицательная"
+    assert product_samsung.price == 180000.0
