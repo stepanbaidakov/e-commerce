f6f6932d4cf8d348defe26fb40e5a9008540e539
Stepan Baidakov <stepanbaidakov@icloud.com>
2025-08-28 15:19:04 +0300

    started

--- src/classes.py
+++ src/classes.py
@@ -66,3 +66,20 @@ class Category:
         for product in self.__products:
             result.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.")
         return result
+
+
+class Smartphone(Product):
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
